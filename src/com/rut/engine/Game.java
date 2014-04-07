6bb964ea2dd6b8a6213610c3fd4405838ed1eb23
StevenOppel <snapproductions@live.com>
2014-04-06 21:34:10 -0400

    Finnished the bufferstrategy today and filled the screen with black - day 5 on the Game engine.

--- src/com/rut/engine/Game.java
+++ src/com/rut/engine/Game.java
@@ -1,7 +1,10 @@
 package com.rut.engine;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -23,10 +26,13 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	public synchronized void start() {
+		running = true;
 		thread = new Thread(this, "Display");
+		thread.start();
 	}
 
 	public synchronized void stop() {
+		running = false;
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
@@ -46,7 +52,17 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
 		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.dispose();
+		bs.show();
 	}
 	
 	public static void main(String[] args) {
